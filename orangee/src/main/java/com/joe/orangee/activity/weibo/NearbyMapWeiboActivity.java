40a1843e313bf2108683511fea7f31088dafcc43
qiaorongzhu <qiaorz@qq.com>
2014-12-11 11:25:40 +0800

    secret

--- orangee/src/main/java/com/joe/orangee/activity/weibo/NearbyMapWeiboActivity.java
+++ orangee/src/main/java/com/joe/orangee/activity/weibo/NearbyMapWeiboActivity.java
@@ -2,11 +2,14 @@ package com.joe.orangee.activity.weibo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.joe.orangee.R;
 import com.joe.orangee.fragment.weibo.NearbyWeiboMapFragment;
+import com.joe.orangee.util.Utils;
 
 public class NearbyMapWeiboActivity extends ActionBarActivity {
 
@@ -14,6 +17,11 @@ public class NearbyMapWeiboActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby_map_weibo);
+        View contentView=findViewById(R.id.content_layout);
+        Utils.setTopPadding(this, contentView);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Utils.setActionBarStyle(getSupportActionBar());
         getFragmentManager()
         .beginTransaction()
         .replace(R.id.map_status_cocntainer, new NearbyWeiboMapFragment())

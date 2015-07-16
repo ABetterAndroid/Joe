c026056c7defb66c1315ddf4d5e43f611aa1dbb3
qiaorongzhu <qiaorz@qq.com>
2015-07-16 11:37:40 +0800

    原定位jar包删除

--- orangee/src/main/java/com/joe/orangee/activity/image/ImageBrowseActivity.java
+++ orangee/src/main/java/com/joe/orangee/activity/image/ImageBrowseActivity.java
@@ -79,7 +79,7 @@ public class ImageBrowseActivity extends BaseActivity {
                 break;
             case R.id.action_collect:
                 String url=(String)mViewPager.findViewWithTag(mViewPager.getCurrentItem()).findViewById(R.id.pic_photo).getTag();
-                Toast.makeText(this, "ͼƬ�ղسɹ�",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "图片收藏成功",Toast.LENGTH_SHORT).show();
                 final PictureCollection collection=new PictureCollection(url, status);
                 if (mOpenHelper==null){
 
